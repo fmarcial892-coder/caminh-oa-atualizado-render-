9bfb8778e75c469a6923e4a08c446c6c59c0236d
fmarcial892-coder <fmarcial892@gmail.com>
2026-09-04 10:56:59 -0300

    Forca carregamento das fotos reais do catalogo

--- photo-fix.js
+++ photo-fix.js
@@ -1,16 +1,15 @@
 (() => {
-  // Somente os produtos identificados pelo usuário como sem foto.
-  // Os demais produtos não são alterados.
+  // Fotos reais dos produtos identificados como sem foto.
   const photoFixes = {
-    r4: '/produto-imagem/r4', r5: '/produto-imagem/r5',
-    p2: '/produto-imagem/p2', p3: '/produto-imagem/p3', p4: '/produto-imagem/p4',
-    p5: '/produto-imagem/p5', p6: '/produto-imagem/p6', p7: '/produto-imagem/p7', p9: '/produto-imagem/p9',
-    m1: '/produto-imagem/m1', m2: '/produto-imagem/m2', m3: '/produto-imagem/m3', m4: '/produto-imagem/m4',
-    m5: '/produto-imagem/m5', m6: '/produto-imagem/m6', m7: '/produto-imagem/m7', m8: '/produto-imagem/m8',
-    m9: '/produto-imagem/m9', m10: '/produto-imagem/m10',
-    motor: '/produto-imagem/motor',
-    s1: '/produto-imagem/s1', s2: '/produto-imagem/s2', s4: '/produto-imagem/s4',
-    s6: '/produto-imagem/s6', s9: '/produto-imagem/s9'
+    r4: '/produto-imagem/r4?v=20260904-3', r5: '/produto-imagem/r5?v=20260904-3',
+    p2: '/produto-imagem/p2?v=20260904-3', p3: '/produto-imagem/p3?v=20260904-3', p4: '/produto-imagem/p4?v=20260904-3',
+    p5: '/produto-imagem/p5?v=20260904-3', p6: '/produto-imagem/p6?v=20260904-3', p7: '/produto-imagem/p7?v=20260904-3', p9: '/produto-imagem/p9?v=20260904-3',
+    m1: '/produto-imagem/m1?v=20260904-3', m2: '/produto-imagem/m2?v=20260904-3', m3: '/produto-imagem/m3?v=20260904-3', m4: '/produto-imagem/m4?v=20260904-3',
+    m5: '/produto-imagem/m5?v=20260904-3', m6: '/produto-imagem/m6?v=20260904-3', m7: '/produto-imagem/m7?v=20260904-3', m8: '/produto-imagem/m8?v=20260904-3',
+    m9: '/produto-imagem/m9?v=20260904-3', m10: '/produto-imagem/m10?v=20260904-3',
+    motor: '/produto-imagem/motor?v=20260904-3',
+    s1: '/produto-imagem/s1?v=20260904-3', s2: '/produto-imagem/s2?v=20260904-3', s4: '/produto-imagem/s4?v=20260904-3',
+    s6: '/produto-imagem/s6?v=20260904-3', s9: '/produto-imagem/s9?v=20260904-3'
   };
 
   const apply = () => {
