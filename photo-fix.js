0f8f0fac30f73a5dc31c8285186780a298db5a3e
fmarcial892-coder <fmarcial892@gmail.com>
2026-09-05 17:38:17 -0300

    Restaura a foto do pneu DRC LS601 sem alterar o restante do site

--- photo-fix.js
+++ photo-fix.js
@@ -1,8 +1,9 @@
 (() => {
-  // Fotos reais dos produtos identificados como sem foto.
+  // Correção de foto do produto que estava sem carregamento.
   const photoFixes = {
     r4: '/produto-imagem/r4?v=20260904-3', r5: '/produto-imagem/r5?v=20260904-3',
-    p2: '/produto-imagem/p2?v=20260904-3', p3: '/produto-imagem/p3?v=20260904-3', p4: '/produto-imagem/p4?v=20260904-3',
+    p2: '/produto-imagem/p2?v=20260904-3', p3: '/produto-imagem/p3?v=20260904-3',
+    p4: 'https://images.tcdn.com.br/img/img_prod/495545/pneu_29580r225_liso_drc_ls601_18_lonas_152148m_18_1_20260429095435_e643ee9bc289.jpg',
     p5: '/produto-imagem/p5?v=20260904-3', p6: '/produto-imagem/p6?v=20260904-3', p7: '/produto-imagem/p7?v=20260904-3', p9: '/produto-imagem/p9?v=20260904-3',
     m1: '/produto-imagem/m1?v=20260904-3', m2: '/produto-imagem/m2?v=20260904-3', m3: '/produto-imagem/m3?v=20260904-3', m4: '/produto-imagem/m4?v=20260904-3',
     m5: '/produto-imagem/m5?v=20260904-3', m6: '/produto-imagem/m6?v=20260904-3', m7: '/produto-imagem/m7?v=20260904-3', m8: '/produto-imagem/m8?v=20260904-3',
